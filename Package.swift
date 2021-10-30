c28026e1b6f6512f3b887a0cfd68da59355043c9
Yasuhiro Inami <inamiy@gmail.com>
2021-10-30 21:44:42 +0900

    Use Swift Concurrency back-deploy to support macOS 10.15, iOS 13, tvOS 13, and watchOS 6

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "Actomaton",
-    platforms: [.macOS(.v12), .iOS(.v15), .watchOS(.v8), .tvOS(.v15)],
+    platforms: [.macOS(.v10_15), .iOS(.v13), .watchOS(.v6), .tvOS(.v13)],
     products: [
         .library(
             name: "Actomaton",
